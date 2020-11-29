512944e6012cffe5ca85a778f39d37538311f680
Aman Bhardwaj <inblack>
2020-11-29 23:41:20 +0530

    client setup

---
+++ client/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Container, Typography } from '@material-ui/core';
+
+const index = () =>
+{
+  return (
+    <Container>
+      <Typography variant='h1'>hello</Typography>
+    </Container>
+  );
+};
+
+export default index;
