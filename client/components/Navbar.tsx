042ca7fbdba38761bbac36144798ed1b20ac2795
Aman Bhardwaj <inblack>
2020-12-12 11:11:45 +0530

    mui magic

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,60 +1,54 @@
-import { AppBar, Button, IconButton, Toolbar, Typography, makeStyles, Theme, createStyles, List, ListItem, Grid, Box } from "@material-ui/core";
-import { Menu as MenuIcon } from '@material-ui/icons';
+import { Fragment } from 'react';
+import { createStyles, makeStyles, Theme, Grid, AppBar, Toolbar, Typography, Button, IconButton } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 import NextLink from 'next/link';
 
-const useStyles = makeStyles( ( _: Theme ) => createStyles( {
-    root: {
-        flexGrow: 1
-    },
-    title: {
-        flexGrow: 1
-    },
-    clicheFlex: {
-        display: 'flex'
-    }
-} ) );
+const useStyles = makeStyles( ( theme: Theme ) =>
+    createStyles( {
+        root: {
+            flexGrow: 1,
+        },
+        menuButton: {
+            marginRight: theme.spacing( 2 ),
+        },
+        tootlbar: {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between'
+        },
+        menu: {
+            display: 'flex',
+            alignItems: 'center'
+        }
+    } ),
+);
 
 const Navbar = () =>
 {
     const classes = useStyles();
 
     return (
-        <div className={ classes.root }>
-            <AppBar position='static' color='transparent'>
-                <Toolbar>
-                    <Grid container justify='space-between'>
-                        <Grid item>
-                            <Grid container>
-                                <Grid item xs={ 6 }>
-                                    <IconButton edge='start'>
-                                        <MenuIcon />
-                                    </IconButton>
-                                </Grid>
-                                <Grid item xs={ 6 }>
-                                    <Typography className={ classes.title } variant='h5'>
-                                        Amoeba
-                                    </Typography>
-                                </Grid>
-                            </Grid>
-                        </Grid>
-                        <Grid item>
-                            <List>
-                                <ListItem>
-                                    <NextLink href='/login'>
-                                        <a>
-                                            <Button variant='contained'>
-                                                Login
-                                    </Button>
-                                        </a>
-                                    </NextLink>
-                                </ListItem>
-                            </List>
-                        </Grid>
+        <Fragment>
+            <AppBar position="static" color='transparent'>
+                <Toolbar className={ classes.tootlbar }>
+                    <Grid className={ classes.menu }>
+                        <IconButton edge="start" className={ classes.menuButton } color="inherit" aria-label="menu">
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6">
+                            Heathens
+                    </Typography>
+                    </Grid>
+                    <Grid>
+                        <NextLink href='/login' passHref>
+                            <Button color='inherit'>Login</Button>
+                        </NextLink>
                     </Grid>
                 </Toolbar>
             </AppBar>
-        </div>
+        </Fragment>
     );
-};
+};;
 
 export default Navbar;
+
--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,9 +1,20 @@
+import { useEffect } from 'react';
 import { ThemeProvider, CssBaseline } from '@material-ui/core';
 import '../styles/globals.css';
 import { theme } from '../styles/styles';
 
 function MyApp ( { Component, pageProps } )
 {
+  useEffect( () =>
+  {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector( '#jss-server-side' );
+    if ( jssStyles )
+    {
+      jssStyles.parentElement.removeChild( jssStyles );
+    }
+  }, [] );
+
   return (
     <ThemeProvider theme={ theme }>
       <CssBaseline />
